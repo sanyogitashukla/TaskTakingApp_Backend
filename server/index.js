14b403c496d074614a1d5e8431fbc146c7ceeb1c
sanyogitashukla <sanyogita.shukla0215@gmail.com>
2026-06-05 18:08:47 +0530

    Add a message to Serverside

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ import app from "./app.js";
 
 const port =process.env.PORT || 5002;
 
+app.get("/",(req,res)=>{
+  res.send("welcome!! try in URL /api/tasks then get the task here if you want")
+})
+
 app.listen(port, () => {
   console.log(
     `Server running on port ${port}`
